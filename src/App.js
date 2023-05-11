e27d1342d296f8047701360e80e86ed6cf6486ac
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-05-11 15:11:54 -0700

    connected update and delete

--- src/App.js
+++ src/App.js
@@ -1,72 +1,115 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react"
 import { Routes, Route, Form } from "react-router-dom"
-import Homepage from "./pages/Homepage";
-import Header from "./components/Header";
-import NotFound from "./pages/NotFound";
-import ApartmentEdit from "./pages/ApartmentEdit";
-import ApartmentIndex from "./pages/ApartmentIndex";
-import ApartmentNew from "./pages/ApartmentNew";
-import ApartmentShow from "./pages/ApartmentShow";
-import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage"
+import Header from "./components/Header"
+import NotFound from "./pages/NotFound"
+import ApartmentEdit from "./pages/ApartmentEdit"
+import ApartmentIndex from "./pages/ApartmentIndex"
+import ApartmentNew from "./pages/ApartmentNew"
+import ApartmentShow from "./pages/ApartmentShow"
+import Footer from "./components/Footer"
 import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
-import './App.css'
+import "./App.css"
 import mockApartments from "./mockApartments"
 import mockUsers from "./mockUsers"
-import MyApartments from './pages/MyApartments';
-
+import MyApartments from "./pages/MyApartments"
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState(mockUsers[null]) //Simulated Login
-  const [apartments, setApartments] = useState([])
-    useEffect(() => {
-      readApartment()
-    }, [])
+	const [currentUser, setCurrentUser] = useState(mockUsers[null]) 
+  
+	const [apartments, setApartments] = useState([])
+	useEffect(() => {
+		readApartment()
+	}, [])
 
-  const readApartment = () => {
-    fetch('http://localhost:3000/apartments')
-    .then(response => response.json())
-    .then(payload => {
-      setApartments(payload)
-    })
-    .catch(error => console.log("Apartment read errors ", error))
-  }
+	const readApartment = () => {
+		fetch("http://localhost:3000/apartments")
+			.then((response) => response.json())
+			.then((payload) => {
+				setApartments(payload)
+			})
+			.catch((error) => console.log("Apartment read errors ", error))
+	}
 
-  const createApartment = (createdApartment) => {
-    fetch('http://localhost:3000/apartments', {
-      body: JSON.stringify(createdApartment),
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST"
-    })
-    .then(response => response.json())
-    .then(() => readApartment())
-    .catch(error => console.log("Create apartment errors: ", error))
-  }
+	const createApartment = (createdApartment) => {
+		fetch("http://localhost:3000/apartments", {
+			body: JSON.stringify(createdApartment),
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+		})
+			.then((response) => response.json())
+			.then(() => readApartment())
+			.catch((error) => console.log("Create apartment errors: ", error))
+	}
 
+	const updateApartment = (apartment, id) => {
+		fetch(`http://localhost:3000/apartments/${id}`, {
+			body: JSON.stringify(apartment),
 
-  return (
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "PATCH",
+		})
+			.then((response) => response.json())
+			.then(() => readApartment())
+			.catch((errors) => console.log("Apartment update errors:", errors))
+	}
 
-    <>
-    
-    <Header current_user={currentUser}/>
-    <Routes>
-    <Route path='/' element={<Homepage />} />
-    <Route path='/apartmentedit/:id' element={<ApartmentEdit apartments={apartments} />} />
-    <Route path='/signin' element={<SignIn />} />
-    <Route path='/signup' element={<SignUp />} />
-    <Route path='/apartmentindex' element={<ApartmentIndex apartments={apartments}/>} />
-    <Route path='/apartmentshow/:id' element={<ApartmentShow apartments={apartments}/>} />
-    <Route path='/myapartments' element={<MyApartments apartments={apartments} current_user={currentUser}/>} />
-    <Route path='/apartmentnew' element={<ApartmentNew createApartment={createApartment}/>} />
-    <Route path='*' element={<NotFound />} />
-    </Routes>
-    <Footer />
-    
-    </>
-    
-  );
+	const deleteApartment = (id) => {
+		fetch(`http://localhost:3000/apartments/${id}`, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "DELETE",
+		})
+			.then((response) => response.json())
+			.then(() => readApartment())
+			.catch((errors) => console.log("delete errors:", errors))
+	}
+
+	return (
+		<>
+			<Header current_user={currentUser} />
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route
+					path="/apartmentedit/:id"
+					element={
+						<ApartmentEdit
+							apartments={apartments}
+							updateApartment={updateApartment}
+						/>
+					}
+				/>
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route
+					path="/apartmentindex"
+					element={<ApartmentIndex apartments={apartments} />}
+				/>
+				<Route
+					path="/apartmentshow/:id"
+					element={<ApartmentShow apartments={apartments} deleteApartment={deleteApartment} />}
+				/>
+				<Route
+					path="/myapartments"
+					element={
+						<MyApartments apartments={apartments} current_user={currentUser} />
+					}
+				/>
+				<Route
+					path="/apartmentnew"
+					element={<ApartmentNew createApartment={createApartment} />}
+				/>
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</>
+	)
 }
 
 export default App
